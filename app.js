ad85d106faedfc9f3c7ae7bb2834a5543c275bfb
tanujap148 <tanujap148@gmail.com>
2017-05-15 01:00:10 +0530

    session5assign1-code

---
+++ app.js
@@ -0,0 +1,5 @@
+var name = "Smith";
+var age = 40;
+var dateofbirth = "03/03/1977";
+var placeofbirth = "London";
+document.getElementById("details").innerHTML= "Name: "+name+"<br>Age: "+age+"<br>Date Of Birth: "+dateofbirth+"<br>Place of birth: "+placeofbirth; // displaying the details of employee
